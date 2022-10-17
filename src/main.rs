3f66244e3a745dce7c07c5ead741b31b19d40efc
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-17 08:34:06 +0300

    created enum with two different enum variants(instances)

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,15 @@
 fn main() {
-    
-    // first struct instance
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
 
-    //second struct instance
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-
-
-    println!("we have 2 struct instances: v4: {:#?} and v6: {:#?}",home, loopback);
+    println!("we have 2 struct instances: v4: {:?} and v6: {:?}",home, loopback);
 }
 
 //enum defination
 #[derive(Debug)]
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
-// struct defination that implements above enum
-#[derive(Debug)]
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
+
