2f6d9ae02d42958eee49f002d8cd6c9f763ca8a4
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-17 08:48:17 +0300

    added example with two enum variants of different data types

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let home = IpAddrKind::V4(String::from("127.0.0.1"));
-    let loopback = IpAddrKind::V6(String::from("::1"));
+    let home = IpAddr::V4(127,0,0,1);
+    let loopback = IpAddr::V6(String::from("::1"));
 
-    println!("we have 2 struct instances: v4: {:?} and v6: {:?}",home, loopback);
+    println!("we have 2 enum variants returning data of different data types: v4: {:?} and v6: {:?}",home, loopback);
 }
 
 //enum defination
 #[derive(Debug)]
-enum IpAddrKind {
-    V4(String),
+enum IpAddr {
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
