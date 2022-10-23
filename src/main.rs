397ede4b6e8763c0496fd30001682b25a5b69283
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-23 13:57:54 +0300

    added example that shows how the placeholder _ takes care of all other scenarios that need not be catered for

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
+/*
+The _ placeholder is used as a pattern to cater for all the other scenarios that are not defined in the preceeding patterns */
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-    println!("The value of five is: {:#?}", &five);
-    println!("The value of six is: {:?}", &six);
-    println!("The value of none is: {:?}", &none);
+//    let some_u8_value = 7u8;
+//    match some_u8_value {
+//     1 => println!("one"),
+//     3 => println!("three"),
+//     5 => println!("five"),
+//     7 => println!("seven"),
+//     _ => println!(),
+//    }
+
+   let some_u8_value = Some(3u8);
+   match some_u8_value {
+    Some(1) => println!("one"),
+    Some(3) => println!("three"),
+    Some(5) => println!("five"),
+    Some(7) => println!("seven"),
+    _ => println!(),
+   }
+
 }
 
 
-fn plus_one(x: Option<i32>) -> Option<i32>{
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
-}
 
 
 
