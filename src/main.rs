fc0386cd767fce66e235da3854c4ef6a3e551522
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-23 14:08:48 +0300

    this

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,10 @@
-/*
-The _ placeholder is used as a pattern to cater for all the other scenarios that are not defined in the preceeding patterns */
-fn main() {
-//    let some_u8_value = 7u8;
-//    match some_u8_value {
-//     1 => println!("one"),
-//     3 => println!("three"),
-//     5 => println!("five"),
-//     7 => println!("seven"),
-//     _ => println!(),
-//    }
 
-   let some_u8_value = Some(3u8);
-   match some_u8_value {
-    Some(1) => println!("one"),
-    Some(3) => println!("three"),
-    Some(5) => println!("five"),
-    Some(7) => println!("seven"),
-    _ => println!(),
-   }
+fn main() {
+    //The if let example here is used when we only want to cater for one pattern and do nothing for the other patterns
+    let some_u8_value = Some(3u8);
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
 
 }
 
